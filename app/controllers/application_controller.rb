3bc70679d1a775af85256b6850436ab53b2edf12
xiangma0531 <shoma0531@outlook.com>
2021-11-28 11:35:56 +0900

    application_controller.rb編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
 class ApplicationController < ActionController::Base
+  before_action: :authenticate_admin!
+  before_action: :authenticate_user!
+  before_action: :configure_permitted_parameters_admin, if: :devise_controller?
+  before_action: :configure_permitted_parameters_user, if: :devise_controller?
+  before_action: :configure_permitted_parameters_admin_edit, if: :devise_controller?
+  before_action: :configure_permitted_parameters_user_edit, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters_admin
+    devise_parameter_sanitizer.permit(:sign_up, keys: :name)
+  end
+
+  def configure_permitted_parameters_admin_edit
+    devise_parameter_sanitizer.permit(:account_update, keys: :name)
+  end
+  
+  def configure_permitted_parameters_user
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :grade_id, :subject_id, :course_id, :introduction])
+  end
+
+  def configure_permitted_parameters_user_edit
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :grade_id, :subject_id, :course_id, :introduction])
+  end
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      root_path
+    when User
+      root_path
+    end
+  end
 end

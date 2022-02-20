cfc26a47fa1bc87d7d0b089a7b3ca7cc735f02ca
xiangma0531 <shoma0531@outlook.com>
2022-02-20 21:22:48 +0900

    relationshipsコントローラーのcreate・destroyアクション定義

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,16 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user
+
+  def create
+    following = current_user.follow(@user)
+  end
+
+  def destroy
+    following = current_user.unfollow(@user)
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
 end

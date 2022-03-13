e0f7d1ea62b66e5c20f2eab383de18734f2309a8
xiangma0531 <shoma0531@outlook.com>
2022-03-13 15:33:18 +0900

    コメント機能local戻し、自分の投稿へのコメント不可対応

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,14 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
-    @source = Source.find(params[:source_id])
+    # @source = Source.find(params[:source_id])
     if @comment.save
-      CommentChannel.broadcast_to @source, {comment: @comment, user: @comment.user}
+      redirect_to source_path(@comment.source)
+      # CommentChannel.broadcast_to @source, {comment: @comment, user: @comment.user}
     else
-      @error_comment = @comment
+      # @error_comment = @comment
+      @source = @comment.source
+      @comments = @source.comments
       render "sources/show"
     end
   end

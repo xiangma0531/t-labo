ae98165a610da4973afa2ffc18dc74a80f6a30d5
xiangma0531 <shoma0531@outlook.com>
2022-10-29 23:56:42 +0900

    sourcesテーブルの科目IDのNOTNULL制約解除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_06_062923) do
+ActiveRecord::Schema.define(version: 2022_10_29_143517) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -104,7 +104,7 @@ ActiveRecord::Schema.define(version: 2022_03_06_062923) do
     t.string "title", null: false
     t.integer "grade_id", null: false
     t.integer "subject_id", null: false
-    t.integer "course_id", null: false
+    t.integer "course_id"
     t.text "content", null: false
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false

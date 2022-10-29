f9fa9167f9b8a963946d5950023e58a46e06fe50
xiangma0531 <shoma0531@outlook.com>
2022-10-29 23:02:36 +0900

    記事一覧表示情報調整

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,9 +1,9 @@
 class Grade < ActiveHash::Base
   self.data = [
-    { id: 1, name: '幼稚園・保育園'}, { id: 2, name: '小学校'},
-    { id: 3, name: '中学校'}, { id: 4, name: '高等学校'},
-    { id: 5, name: '特別支援学校'}, { id: 6, name: '大学'},
-    { id: 7, name: 'その他'}
+    { id: 1, name: '幼稚園・保育園', abb_name: '幼・保'}, { id: 2, name: '小学校', abb_name: '小学校'},
+    { id: 3, name: '中学校', abb_name: '中学校'}, { id: 4, name: '高等学校', abb_name: '高校'},
+    { id: 5, name: '特別支援学校', abb_name: '特支'}, { id: 6, name: '大学', abb_name: '大学'},
+    { id: 7, name: 'その他', abb_name: 'その他'}
   ]
   include ActiveHash::Associations
   has_many :sources

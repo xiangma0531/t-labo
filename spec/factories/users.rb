7c1f84a7d6b4c60638bb6918286d876e35b41406
xiangma0531 <shoma0531@outlook.com>
2023-07-01 14:51:15 +0900

    FactoryBot(:user)ファイル作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    name {}
+    email {}
+    password {}
+    password_confirmation {}
+    grade_id {}
+    subject_id {}
+    course_id {}
+    introduction {}
+  end
+end

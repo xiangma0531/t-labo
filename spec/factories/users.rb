b6942a102b74e699756e65b402e768d5a448dde1
xiangma0531 <shoma0531@outlook.com>
2023-07-01 14:57:51 +0900

    Userモデルテスト用データ用意

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :user do
-    name {}
-    email {}
-    password {}
-    password_confirmation {}
-    grade_id {}
-    subject_id {}
-    course_id {}
-    introduction {}
+    name { Faker::Name.name }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    grade_id { Faker::Number.between(from: 1, to: 7) }
+    subject_id { Faker::Number.between(from: 1, to: 15) }
+    course_id { Faker::Number.between(from: 1, to: 30) }
+    introduction { Faker::Lorem.sentence }
   end
 end

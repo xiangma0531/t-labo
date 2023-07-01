fb7741f93f1806837a7744138696908b0698a034
xiangma0531 <shoma0531@outlook.com>
2023-07-01 14:58:56 +0900

    Userデータbuild

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
     # 正常系
     context '新規登録できる場合' do
       it 'name、email、password、password_confirmation、grade_id、subject_id、course_id、introductionがあれば登録できる' do

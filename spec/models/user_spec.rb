36c9e555e2f5df8ba0e2cc090f2f67e32147f3e0
xiangma0531 <shoma0531@outlook.com>
2023-07-01 14:46:30 +0900

    example整理

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    # 正常系
+    context '新規登録できる場合' do
+      it 'name、email、password、password_confirmation、grade_id、subject_id、course_id、introductionがあれば登録できる' do
+        
+      end
+
+      it 'name、email、password、password_confirmationがあれば、grade_id、subject_id、course_id、introductionが未入力でも登録できる' do
+
+      end
+    end
+
+    # 異常系
+    context '新規登録できない場合' do
+      it 'nameが空では登録できない' do
+
+      end
+
+      it 'emailが空では登録できない' do
+        
+      end
+
+      it 'passwordが空では登録できない' do
+        
+      end
+
+      it 'passwordとpassword_confirmationが一致していなければ登録できない' do
+        
+      end
+    end
+  end
 end
